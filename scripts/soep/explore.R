835613e1e47fc7c95d90b26bae75f9e471953860
Benjamin Dahmen <bendahmen@gmail.com>
2021-03-12 10:42:49 +0000

    created folders and exploratory SOEP script

---
+++ scripts/soep/explore.R
@@ -0,0 +1,10 @@
+#PACKAGES
+installation_needed  <- FALSE
+loading_needed <- TRUE
+package_list <- c('foreign')
+if(installation_needed){install.packages(package_list, repos='http://cran.us.r-project.org')}
+if(loading_needed){lapply(package_list, require, character.only = TRUE)}
+
+rm(list=ls()) #clear workspace
+#SET WD
+
